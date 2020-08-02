24ae60f92133fdb1c649eeb50f51374f62641283
pronist <pronist@naver.com>
2020-08-02 17:50:54 +0900

    add: width --configuration

--- assets/js/h.js
+++ assets/js/h.js
@@ -30,42 +30,4 @@ module.exports = class {
       $notify.remove()
     }
   }
-
-  /**
-  * Image Alignment
-  *
-  * @param {string} block
-  * @param {number} contentWidth
-  * @param {number} maxWidth
-  */
-  static alignImageBlock (block, contentWidth, maxWidth) {
-    $(block).each(function () {
-      let width = $(this).data('origin-width')
-      width = width > maxWidth ? maxWidth : width
-
-      $(this).css({ width, 'max-width': 'none' })
-
-      const marginLeft = (contentWidth - width) / 2
-      if (marginLeft < 0) {
-        $(this).css('margin-left', marginLeft)
-      }
-    })
-  }
-
-  /**
-   * Add Id attribute in Heading elements
-   *
-   * @param {string} parent
-   * @param {string} selector
-   */
-  static wrapWithAnchor (parent, selector) {
-    const $parent = $(parent)
-    $parent.find(selector).each(function () {
-      const $heading = $(this)
-      const anchor = encodeURIComponent($heading.text())
-      const $a = $('<a></a>').attr('href', '#' + anchor).text($heading.text())
-
-      $heading.attr('id', anchor).empty().append($a)
-    })
-  }
 }
