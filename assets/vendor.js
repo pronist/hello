ed9fb24763dea4b17998bc1ae06d5e2173914a77
pronist <pronist@naver.com>
2023-01-20 02:30:43 +0900

    Update cover styles

--- assets/vendor.js
+++ assets/vendor.js
@@ -1,13 +1,27 @@
 /** @see https://github.com/aFarkas/lazysizes */
 import 'lazysizes'
 
-/** @see https://swiperjs.com */
-import Swiper from 'swiper'
-import 'swiper/css'
-
-window.Swiper = Swiper
-
 /** @see https://highlightjs.org */
 import hljs from 'highlight.js'
 
 window.hljs = hljs
+
+/** @see https://swiperjs.com */
+import Swiper, { Navigation } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  loop: true,
+  autoplay: {
+    delay: 3000
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+})
+
+window.swiper = swiper
