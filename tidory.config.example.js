d436afcddec441cb46b7fc256faf42794879f764
pronist <pronist@naver.com>
2023-08-01 00:57:41 +0900

    Update tidory config

--- tidory.config.example.js
+++ tidory.config.example.js
@@ -47,7 +47,57 @@ module.exports = {
       scrollspy: true,
       hljs: 'xcode',
       hljsDark: 'vs2015'
-    }
+    },
+
+    /**
+     * Cover Settings
+     */
+    coverSettings: [
+      {
+        description: '슬라이드쇼',
+        index: 0,
+        name: 'slideshow',
+        title: '슬라이드쇼',
+        dataType: 'RECENT',
+        data: {
+          category: 'ALL',
+          size: '5'
+        }
+      },
+      {
+        description: '리스트',
+        index: 0,
+        name: 'list',
+        title: '리스트',
+        dataType: 'RECENT',
+        data: {
+          category: 'ALL',
+          size: '5'
+        }
+      },
+      {
+        description: '그리드',
+        index: 0,
+        name: 'grid',
+        title: '그리드',
+        dataType: 'RECENT',
+        data: {
+          category: 'ALL',
+          size: '5'
+        }
+      },
+      {
+        description: '지그재그',
+        index: 0,
+        name: 'zigzag',
+        title: '지그재그',
+        dataType: 'RECENT',
+        data: {
+          category: 'ALL',
+          size: '5'
+        }
+      }
+    ]
   },
 
   alias: {
