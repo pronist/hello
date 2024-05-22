8339fd27dc1b54c91e7b6cf6714b63c736aa6e39
pronist <pronist@naver.com>
2024-05-22 12:02:54 +0900

    Refactor dark mode class

--- images/bootstrap.js
+++ images/bootstrap.js
@@ -1,18 +1,18 @@
-class Theme {
+class DarkMode {
   /**
-   * Create Theme Instance
+   * Create DarkMode Instance
    */
   constructor () {
     /**
-     * Class name for dark mode
+     * Class name
      *
      * @see https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
      * @type {string}
      */
-    this.className = 'dark'
+    this.class = 'dark'
 
     /**
-     * Key name in LocalStorage for dark mode
+     * Key name in LocalStorage
      *
      * @type {string}
      */
@@ -20,46 +20,39 @@ class Theme {
   }
 
   /**
-   * Is dark
+   * Preferred
    *
    * @return {boolean}
    */
-  isDark () {
-    const os = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Y' : 'N'
+  preferred () {
+    const os = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Y' : 'N'
     const signal = localStorage.getItem(this.key) || os
 
     return signal === 'Y'
   }
 
   /**
-   * Add new theme
-   *
-   * @param {string} className
+   * On
    */
-  add (className) {
-    document.documentElement.classList.add(className)
+  on () {
+    document.documentElement.classList.add(this.class)
   }
 
   /**
-   * Set Default Theme
-   */
-  setDefault () {
-    this.isDark() && this.add(this.className)
-  }
-
-  /**
-   * Toggle Theme
+   * Toggle
    */
   toggle () {
-    localStorage.setItem(this.key, document.documentElement.classList.contains(this.className) ? 'N' : 'Y')
-    document.documentElement.classList.toggle(this.className)
+    localStorage.setItem(this.key, document.documentElement.classList.contains(this.class) ? 'N' : 'Y')
+    document.documentElement.classList.toggle(this.class)
   }
 }
 
-const theme = new Theme()
-theme.setDefault()
+const darkMode = new DarkMode()
+const preferredDarkMode = darkMode.preferred()
 
-window.TTDARK = theme.isDark()
+preferredDarkMode && darkMode.on()
+
+window.TTDARK = preferredDarkMode
 
 /**
  * Console Issues
