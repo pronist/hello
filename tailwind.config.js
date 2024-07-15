18064412ebd9bec0feda14aff770ca6e37642078
pronist <pronist@naver.com>
2024-07-15 18:59:38 +0900

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app.pug', './index.pug', './views/**/*.pug'],
+  content: ['./app.pug', './index.pug', './views/**/*.pug', './assets/**/*.pug'],
   darkMode: 'class',
   theme: {
     screens: {
